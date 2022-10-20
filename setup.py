47bef96dcb9c49e712965f4e3ca43e295870572e
himanshu <himanshu@algodommedia.com>
2022-10-20 18:31:43 +0530

    deleted .github folder

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='followerauditapi',
-    version='0.0.4',
+    version='0.0.6',
     packages=['followeraudit'],
     description='Python library to access followeraudit API',
     long_description=long_description,
