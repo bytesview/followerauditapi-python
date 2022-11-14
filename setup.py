3957df860e3252b09446e30efa5f36315495a452
Himanshu Gautam <105356403+himanshu-git1@users.noreply.github.com>
2022-11-14 14:39:28 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='followerauditapi',
-    version='0.0.7',
+    version='0.0.8',
     packages=['followeraudit'],
     description='Python library to access followeraudit API',
     long_description=long_description,
