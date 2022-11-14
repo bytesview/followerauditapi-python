8c635418e0109ed9606eedc88e7d8e3bd6c593fe
himanshu <himanshu@algodommedia.com>
2022-11-14 10:30:58 +0530

    changed version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='followerauditapi',
-    version='0.0.6',
+    version='0.0.7',
     packages=['followeraudit'],
     description='Python library to access followeraudit API',
     long_description=long_description,
